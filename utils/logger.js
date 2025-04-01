dd880ab6d09a062cc703bf141405a49d46d13788
pawelwilga <pawel@wilga..net.pl>
2025-04-01 22:25:52 +0200

    zaimplementowany plik logger, plik routing zmodyfikowany tak, aby używał loggera

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,27 @@
 // 🔄 Refactoro the Changer  
 // Przenieś teraz odpowiednie wywołania logów z routing.js i zastąp tam logowanie bezpośrednie wywołaniem tych funkcji.
 
-const getInfoLog = () => {};
+const getInfoLog = (url, method) => {
+  console.log(
+    `[INFO] (${new Date(Date.now()).toUTCString()}): ${method} - ${url}`
+  );
+};
 
-const getErrorLog = () => {};
+const getErrorLog = (url) => {
+  console.warn(
+    `[ERROR] (${new Date(
+      Date.now()
+    ).toUTCString()}): requested url ${url} doesn't exist.`
+  );
+};
 
-const getProcessLog = () => {};
+const getProcessLog = () => {
+  console.log(
+    `[PROCESS] (${new Date(
+      Date.now()
+    ).toUTCString()}): logout has been initiated and the application will be closed.`
+  );
+};
 
 module.exports = {
   getInfoLog,
