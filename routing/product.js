35abd6ce3f801f4806b5f819ded3db75ae9d04c2
pawelwilga <pawel@wilga..net.pl>
2025-04-02 16:20:50 +0200

    Zmiany w zakresie wyświetlania produktu

--- routing/product.js
+++ routing/product.js
@@ -3,6 +3,7 @@ const { STATUS_CODE } = require("../constants/statusCode");
 const path = require('path');
 const express = require('express');
 const logger = require('../utils/logger');
+const rnpp = require('../views/renderNewProductPage');
 
 const router = express.Router();
 
@@ -22,46 +23,23 @@ router.post('/add', (req, res) => {
     }
   );
 });
+router.get('/new', (req, res) => {
+  fileSystem.readFile(
+    path.join(__dirname, '../', 'product.txt'),
+    'utf-8',
+    (err, data) => {
+      if (err) {
+        res.status(STATUS_CODE.NOT_FOUND).send(rnpp(null));
+      } else {
+        const productData = JSON.parse(data);
+        res.send(rnpp(productData));
+      }
+    }
+  );
+});
 router.use((req, res) => {
   logger.getErrorLog(req.url);
   res.status(STATUS_CODE.NOT_FOUND).sendFile(path.join(__dirname, '../views', '404.html'));
 });
-
-const productRouting = (request, response) => {
-  const { url, method } = request;
-
-  if (url.includes("add") && method === "POST") {
-    return addNewProduct(request, response);
-  }
-
-  if (url.includes("new")) {
-    return renderNewProductPage(response);
-  }
-
-  console.warn(`ERROR: requested url ${url} doesn't exist.`);
-  return;
-};
-
-const renderNewProductPage = (response) => {
-  fileSystem.readFile("./product.txt", "utf-8", (err, data) => {
-    response.setHeader("Content-Type", "text/html");
-    response.write("<html>");
-    response.write("<head><title>Shop - Newest product</title></head>");
-    response.write("<body>");
-    response.write("<h1>Newest product</h1>");
-    response.write(
-      "<nav><a href='/'>Home</a><br /><a href='/product/add'>Add product</a><br /><a href='/logout'>Logout</a></nav>"
-    );
-
-    if (err) {
-      response.write("<br /><div>No new products available.</div>");
-    } else {
-      response.write(`<br /><div>New product data - ${splittedData}</div>`);
-    }
-
-    response.write("</body>");
-    response.write("</html>");
-  });
-};
 
 module.exports = router;
--- views/renderNewProductPage.js
+++ views/renderNewProductPage.js
@@ -1,5 +1,24 @@
 /* 🏗 Structo the Builder */
 /* Funkcja generująca stronę HTML z najnowszym produktem */
-const renderNewProductPage = () => {};
+const renderNewProductPage = (productData) => {
+    let renderedTemplate = `<!DOCTYPE html>
+<html lang="en">
+<head><title>Shop - Newest product</title></head>
+<body>
+<h1>Newest product</h1>
+<nav><a href='/home'>Home</a><br /><a href='/product/add'>Add product</a><br /><a href='/logout'>Logout</a></nav>
+${ productData === null ? `<div>New product data is uanvailable.</div>` : `<p>Product name: ${productData.name} </p>`}
+${ productData === null ? `` : `<p>Product description: ${productData.description} </p>` }
+</body>
+</html>
+`;
+    /* if (err) {
+      response.write("<br /><div>No new products available.</div>");
+    } else {
+      response.write();
+    } */
+
+    return renderedTemplate;
+};
 
 module.exports = renderNewProductPage;
